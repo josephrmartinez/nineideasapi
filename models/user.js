e5c3dc5731ed48a8ded6ed51974c32dc70d961e1
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-07-24 19:14:45 -0700

    Update user model

--- models/user.js
+++ models/user.js
@@ -12,6 +12,44 @@ const UserSchema = new Schema({
   lists: [{ type: Schema.Types.ObjectId, ref: "List" }]
 });
 
+UserSchema.virtual("totallists").get(function () {
+  return this.lists.length();
+});
+
+UserSchema.virtual("currentstreak").get(function () {
+// Write function to calculate current "streak" of writing lists
+//  function countConsecutiveDates(data) {
+//   let count = 1;
+
+//   // Get the last date in the array
+//   const lastDate = new Date(data[data.length - 1].dateAdded);
+
+//   // Loop through the array backwards, starting from the second to last item
+//   for (let i = data.length - 2; i >= 0; i--) {
+//     const currentDate = new Date(data[i].dateAdded);
+
+//     // Check if the current date is one day before the previous date
+//     if (
+//       currentDate.getDate() === lastDate.getDate() - 1
+//     ) {
+//       count++;
+//       lastDate.setTime(currentDate.getTime()); // Update lastDate to current date
+//     } else {
+//       break; // Stop looping if the dates are not consecutive
+//     }
+//   }
+
+//   return count;
+// }
+
+});
+
+UserSchema.virtual("recordstreak").get(function () {
+// Write function to calculate the longest consecutive streak of days with completed lists
+  return this.lists.length();
+});
+
+
 
 // Export model
 module.exports = mongoose.model("User", UserSchema);
