fed6dabf0d2e86ed1ff5a4ca6fa6041db6f64fc1
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-07-26 17:34:43 -0700

    Update to simplified list model

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const mongoose = require('mongoose');
 const session = require("express-session");
 const cors = require('cors');
+const User = require('/models/user')
 
 // connect to MongoDB
 mongoose.set('strictQuery', false);
@@ -17,6 +18,48 @@ app.use(cors());
 app.use(express.json())
 // Configure express-session
 app.use(session({ secret: "mango", resave: false, saveUninitialized: true }));
+
+passport.use(
+    // LocalStrategy is the common username-password strategy for auth
+    new LocalStrategy(async(username, password, done) => {
+      try {
+        const user = await User.findOne({ username: username });
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        };
+        bcrypt.compare(password, user.password, (err, res)=> {
+            if (res){
+                // passwords match, log user in
+                return done(null, user)
+            } else {
+                // passwords to not match
+                return done(null, false, {message: "Incorrect Password"})
+            }
+        })
+      } catch(err) {
+        return done(err);
+      };
+    })
+  );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async function(id, done) {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch(err) {
+      done(err);
+    };
+  });
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 // CHECK Middleware function to access currentUser variable in all views
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
