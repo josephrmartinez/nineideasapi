a94d584bebabe0f2d7dc9150c6a10395a4d79f1e
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-07-26 17:43:18 -0700

    Update listController

--- controllers/listController.js
+++ controllers/listController.js
@@ -4,39 +4,43 @@ const List = require('../models/list');
 const listController = {
   createList: asyncHandler(async (req, res) => {
     // Implement logic to create a new list based on the request data
-    const newList = new List({ ... });
-    // Save the new list to the database
-    // Example: await newList.save();
-    // Respond with the created list details
-    // Example: res.status(201).json(newList);
+    const newList = new List(
+      { topic: req.body.topic,
+        ideas: req.body.ideas,
+        author: req.user,
+      });
+      await newList.save()
+      res.status(201).json(newList);
   }),
 
   getAllLists: asyncHandler(async (req, res) => {
     // Implement logic to fetch all published lists
-    // Example: const lists = await List.find({ status: 'published' });
-    // Respond with the array of lists
-    // Example: res.json(lists);
+    const lists = await List.find({ status: 'published' })
+    .populate("author")
+    .exec();
+    // LIMIT TO 20 LISTS, IMPLEMENT PAGINATION
+    res.json(lists);
   }),
 
   getListById: asyncHandler(async (req, res) => {
     // Implement logic to find and return a list by ID
-    // Example: const list = await List.findById(req.params.id);
+    const list = await List.findById(req.params.id);
     // Respond with the list details
-    // Example: res.json(list);
+    res.json(list);
   }),
 
   updateList: asyncHandler(async (req, res) => {
     // Implement logic to update the list details based on the request data
-    // Example: const updatedList = await List.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedList = await List.findByIdAndUpdate(req.params.id, req.body, { new: true });
     // Respond with the updated list details
-    // Example: res.json(updatedList);
+    res.json(updatedList);
   }),
 
   deleteList: asyncHandler(async (req, res) => {
     // Implement logic to delete the list by ID
-    // Example: await List.findByIdAndDelete(req.params.id);
+    await List.findByIdAndDelete(req.params.id);
     // Respond with a success message
-    // Example: res.json({ message: 'List deleted successfully' });
+    res.json({ message: 'List deleted successfully' });
   }),
 };
 
