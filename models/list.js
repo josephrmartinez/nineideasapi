6cc6b756ba8c03bbc2a8b38ed38426f5d8a25dc9
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-08-07 17:17:36 -0700

    Update list model

--- models/list.js
+++ models/list.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema;
 const { DateTime } = require("luxon")
 
 const ListSchema = new Schema({
-  topic: { type: Schema.Types.ObjectId, ref: "Topic" },
+  topic: { type: Schema.Types.ObjectId, required: true, ref: "Topic" },
   ideas: [{ type: Schema.Types.ObjectId, ref: "Idea" }],
   dateAdded: { type: Date, default: Date.now },
   timeStarted: { type: Date, default: Date.now },
   timeCompleted: {type: Date },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
+  author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
   likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   status: { type: String, enum: ['draft', 'complete'], default: 'draft' },
