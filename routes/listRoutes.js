c42b42d301e1c2388a29b722e5b69f3c94dc7aa1
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-09-09 17:42:36 -0700

    drop comment model and controller

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const listController = require('../controllers/listController');
-const commentController = require('../controllers/commentController');
 
 // Define the API routes related to lists
 router.get('/', listController.getAllLists);
@@ -10,10 +9,6 @@ router.get('/:id', listController.getListById);
 router.put('/:id', listController.updateList);
 router.patch('/:id', listController.patchUpdateList);
 router.delete('/:id', listController.deleteList);
-router.get('/:id/comments', commentController.getCommentsForList)
-router.post('/:id/comments', commentController.createComment)
-router.put('/:id/comments', commentController.updateComment)
-router.delete('/:id/comments', commentController.deleteComment)
 
 
 // More list-related routes if needed...
